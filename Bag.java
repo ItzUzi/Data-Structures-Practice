12369f25a3c89940d806291befcae24cc4247b69
Uziel Gaeta <ugaeta@cpp.edu>
2021-09-02 13:22:48 -0700

    Added all of the functions from the interface and started implementing some functions

--- Bag.java
+++ Bag.java
@@ -1,3 +1,61 @@
-public class Bag {
+public class Bag<T> implements BagInterface<T> {
+
+    private T[] bag;
+    private int numberOfEntries;
+    private final int DEFAULT_CAPACITY = 25;
+
+    public Bag(int capacity){
+        this.numberOfEntries = 0;
+        @SuppressWarnings("unchecked")
+        T[] tempBag = (T[])new Object[capacity];
+        bag = tempBag;
+    }
+
+    public Bag(){
+        this.numberOfEntries = 0;
+        @SuppressWarnings("unchecked")
+        T[] tempBag = (T[])new Object[DEFAULT_CAPACITY];
+        bag = tempBag;
+    }
+
+    public int getCurrentSize() {
+        return numberOfEntries;
+    }
+
+    public boolean isEmpty() {
+        for (T t : bag) {
+            if(t == null)
+                return true;
+        }
+        return false;
+    }
+
+    public boolean add(T newEntry) {
+        return false;
+    }
+
+    public T remove() {
+        return null;
+    }
+
+    public boolean remove(T anEntry) {
+        return false;
+    }
+
+    public void clear() {
+        
+    }
+
+    public boolean getFrequencyOf(T anEntry) {
+        return false;
+    }
+
+    public boolean contains(T anEntry) {
+        return false;
+    }
+
+    public T[] toArray() {
+        return null;
+    }
     
 }
