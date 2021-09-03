53ff52430be45b1c458697e19a8b543fb74f1567
Uziel Gaeta <ugaeta@cpp.edu>
2021-09-02 23:29:29 -0700

    Added functions with full functionality for reSize(), isFull() and place holder for push

--- Stack.java
+++ Stack.java
@@ -22,11 +22,11 @@ public class Stack<T> {
         if (isEmpty()) {
             return null;
         }
-        
+
         T results = stack[numberOfEntries];
         stack[numberOfEntries] = null;
         numberOfEntries--;
-        
+
         return results;
     }
 
@@ -35,12 +35,56 @@ public class Stack<T> {
 
         if (stack.length == 0)
             return results;
-        
+
         for (T t : stack) {
             if (t != null) {
                 results = false;
             }
         }
-        return results; 
+        return results;
     }
+
+    public T peek(){
+
+        if (isEmpty()) {
+            return null;
+        }
+
+        T results = stack[numberOfEntries];
+
+        return results;
+    }
+
+    public T push(T anEntry){
+        if (isFull()) {
+            reSize();
+        }
+
+        return null;
+    }
+
+    private boolean isFull(){
+
+        boolean results = false;
+
+        if (numberOfEntries == stack.length - 1) {
+            results = true;
+        }
+
+        return results;
+    }
+
+    private void reSize(){
+        int tempLength = stack.length + 1;
+
+        @SuppressWarnings("unchecked")
+        T[] temp = (T[])new Object[tempLength];
+
+        for (int i = 0; i < stack.length; i++) {
+            temp[i] = stack[i];
+        }
+
+        stack = temp;
+    }
+
 }
