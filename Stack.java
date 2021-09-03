0a01cace4065c6188a45e7f450a8b1df8a582b63
Uziel Gaeta <ugaeta@cpp.edu>
2021-09-02 23:09:53 -0700

    Added constuctors for the stack Class and added pop() and isEmpty methods

--- Stack.java
+++ Stack.java
@@ -1,3 +1,46 @@
 public class Stack<T> {
     
+    private T[] stack;
+    private final int DEFAULT_CAPACITY = 25;
+    private int numberOfEntries;
+
+    Stack(){
+        @SuppressWarnings("unchecked")
+        T[] temp = (T[])new Object[DEFAULT_CAPACITY];
+        stack = temp;
+        numberOfEntries = 0;
+    }
+
+    Stack(int capacity){
+        @SuppressWarnings("unchecked")
+        T[] temp = (T[])new Object[capacity];
+        stack = temp;
+        numberOfEntries = 0;
+    }
+
+    public T pop(){
+        if (isEmpty()) {
+            return null;
+        }
+        
+        T results = stack[numberOfEntries];
+        stack[numberOfEntries] = null;
+        numberOfEntries--;
+        
+        return results;
+    }
+
+    public boolean isEmpty(){
+        boolean results = true;
+
+        if (stack.length == 0)
+            return results;
+        
+        for (T t : stack) {
+            if (t != null) {
+                results = false;
+            }
+        }
+        return results; 
+    }
 }
